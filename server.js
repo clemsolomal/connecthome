fa8b8187eb567e1eb1a46252c71a78be8144fe61
clemsolomal <clemsolomal@gmail.com>
2021-05-12 21:17:06 +0200

    second commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app= express();
-app.use(express.static('./dist/maison-Connectee'));
+app.use(express.static('./dist/connecthome'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/maison-Connectee/'}),
+    res.sendFile('index.html', {root: 'dist/connecthome/'}),
 );
 
 app.listen(process.env.PORT || 8080);
