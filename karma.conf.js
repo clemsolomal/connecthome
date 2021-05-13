b3042ae59475ed5d7e884f888b1fbaa1dbfda97b
clemsolomal <clemsolomal@gmail.com>
2021-05-13 12:25:08 +0200

    seventh commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mon-projet-angular'),
+      dir: require('path').join(__dirname, './coverage/connecthome'),
       subdir: '.',
       reporters: [
         { type: 'html' },
